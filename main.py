8365602bdfcdee48d4ccf96ce736ac889044903f
Abishek <yourgithubemail@gmail.com>
2026-03-15 16:03:00 +0530

    Added animated result page redirect

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-# store workflows
 workflow_data = []
 
 class Expense(BaseModel):
@@ -24,35 +23,84 @@ def submit_expense(expense: Expense):
     amount = expense.amount
 
     if amount < 500:
-        status = "❌ Rejected"
+        status = "Rejected ❌"
 
     elif amount < 1000:
-        status = "🎉 Auto Approved"
+        status = "Auto Approved 🎉"
 
     elif amount < 10000:
-        status = "👨‍💼 Manager Approval Required"
+        status = "Manager Approval Required 👨‍💼"
 
     else:
-        status = "💰 Finance Approval Required"
+        status = "Finance Approval Required 💰"
 
-
-    # store workflow entry
-    workflow_entry = {
+    workflow_data.append({
         "name": expense.name,
         "amount": amount,
         "status": status
-    }
-
-    workflow_data.append(workflow_entry)
+    })
 
     return {
-        "name": expense.name,
-        "amount": amount,
         "status": status,
         "active": len(workflow_data)
     }
 
 
 @app.get("/workflows")
-def get_workflows():
+def workflows():
     return workflow_data
+
+
+@app.get("/result/{status}", response_class=HTMLResponse)
+def result_page(status: str):
+
+    return f"""
+    <html>
+
+    <head>
+
+    <title>Workflow Result</title>
+
+    <style>
+
+    body{{
+        font-family:"Times New Roman";
+        background:#0f2027;
+        color:white;
+        text-align:center;
+        margin-top:150px;
+    }}
+
+    .box{{
+        font-size:60px;
+        animation:pop 1s infinite alternate;
+    }}
+
+    @keyframes pop{{
+        from{{transform:scale(1)}}
+        to{{transform:scale(1.2)}}
+    }}
+
+    a{{
+        color:#00c6ff;
+        font-size:20px;
+    }}
+
+    </style>
+
+    </head>
+
+    <body>
+
+    <div class="box">
+    {status}
+    </div>
+
+    <br><br>
+
+    <a href="/">⬅ Back to Workflow Dashboard</a>
+
+    </body>
+
+    </html>
+    """
